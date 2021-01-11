7651bee0ebd8d0744aeb1b4a04a76fb6e3e287f2
jeffery graham <jeffery.lemar.graham@gmail.com>
2021-01-10 19:36:31 -0500

    Mastering Typed Arrays

---
+++ features/arrays.ts
@@ -0,0 +1,22 @@
+const carMakers = ['Ford', 'Mercedes-Benz', 'BMW'];
+const dates = [new Date(), new Date(), new Date()];
+
+const carsBymake = [['Camry', 'S550', 'Series 7']];
+
+//Help w/ inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+//Prevent incomptible values
+// carMakers.push(100);
+
+//Help w/ 'map'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
+
+//Flexible types
+const importantDates: (Date | string)[] = [new Date(), '2021-03-21'];
+
+importantDates.push('2021-03-21');
+importantDates.push(new Date());
