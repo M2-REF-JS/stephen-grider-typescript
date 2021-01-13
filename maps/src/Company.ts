d2ac1745741d2ab27b7abf3db876244fcd32192c
jeffery graham <jeffery.lemar.graham@gmail.com>
2021-01-12 21:39:22 -0500

    Design Patterns with Typescript

---
+++ maps/src/Company.ts
@@ -0,0 +1,32 @@
+import faker from 'faker';
+
+import { Mappable } from './CustomMap';
+
+export class Company implements Mappable {
+  companyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  color: string = 'red';
+
+  constructor() {
+    this.companyName = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: Number(faker.address.latitude()),
+      lng: Number(faker.address.longitude()),
+    };
+  }
+
+  markerContent(): string {
+    return `
+    <div>
+    <h1>Company name: ${this.companyName}</h1>
+    <h3>Company catchphrase: ${this.catchPhrase}</h3>
+    </di>
+    `;
+  }
+}
---
+++ maps/src/User.ts
@@ -0,0 +1,24 @@
+import faker from 'faker';
+
+import { Mappable } from './CustomMap';
+
+export class User implements Mappable {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  color: string = 'red';
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: Number(faker.address.latitude()),
+      lng: Number(faker.address.longitude()),
+    };
+  }
+
+  markerContent(): string {
+    return `User name: ${this.name}`;
+  }
+}
